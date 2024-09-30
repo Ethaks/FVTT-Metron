717ee259b52118337b534923e50c2be9a2ac27ac
Ethaks <ethaks@protonmail.ch>
2024-09-30 15:43:54 +0200

    fix: adjust for grid data nesting

--- src/module/metron.mjs
+++ src/module/metron.mjs
@@ -55,10 +55,10 @@ Hooks.once("setup", () => {
 
 Hooks.once("ready", () => {
   if (game.settings.get(utils.MODULE_ID, "gridUnitSystem") === "metric") {
-    const currentGridUnits = utils.getUnitFromString(game.system.data.gridUnits);
-    if (currentGridUnits === utils.UNITS.FEET && game.system.data.gridDistance === 5) {
-      game.system.data.gridUnits = utils.localize(`UnitsShort.${utils.UNITS.METER}`);
-      game.system.data.gridDistance = 1.5;
+    const currentGridUnits = utils.getUnitFromString(game.system.grid.units);
+    if (currentGridUnits === utils.UNITS.FEET && game.system.grid.distance === 5) {
+      game.system.grid.units = utils.localize(`UnitsShort.${utils.UNITS.METER}`);
+      game.system.grid.distance = 1.5;
     }
   }
 });
